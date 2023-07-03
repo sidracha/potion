03a5e530a92a28e7dd5b95180d63a7e4d749ad7f
sidsail <sidsailcloud@gmail.com>
2023-07-02 17:12:16 -0700

    build

---
+++ build.sh
@@ -0,0 +1,10 @@
+g++ -c -fPIC -std=c++20 -I../lib/boostlib src/utils.cpp src/threading.cpp src/tcpserver_unix.cpp src/request.cpp src/response.cpp src/potion.cpp
+
+g++ utils.o threading.o tcpserver_unix.o response.o request.o potion.o -shared -o bin/libpotion.so
+
+rm potion.o
+rm tcpserver_unix.o
+rm utils.o
+rm request.o
+rm response.o
+rm threading.o
